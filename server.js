591562732306acdee26ec8ea16a43873495fde68
edwardsspell <55387220+edwardsspell@users.noreply.github.com>
2021-11-19 15:26:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express')(),
       zumo = require('azure-mobile-apps')();
+      module.exports = {
+            cors: {
+                  origins: [ 'https://testzumo.azurewebsites.net/' ]
+            },
+            data: {
+              provider: 'mssql',
+              server: 'https://testzumo.azurewebsites.net/',
+              database: 'quickstart',
+              user: 'zumoadmin',
+              password: 'Pchjnpt7ml24hyx!'
+            },
+          logging: {
+              level: 'verbose'
+          }
+      };
 
 zumo.tables.add('TodoItem');
 zumo.tables.add('TodoModel');
